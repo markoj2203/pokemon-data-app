50fa83dfa18f9c6e20ebf2675757a6c0e5bfd71f
unknown <markoj2203@gmail.com>
2020-12-02 15:26:30 +0100

    listing data on homepage by api

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function Content() {
-  return <div className="content flex-center">Content</div>;
+  const [limit, setLimit] = useState(20);
+  const [offset, setOffset] = useState(0);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      await axios
+        .get(
+          `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`
+        )
+        .then((result) => {
+          let arrData = result.data.results;
+          setData(arrData);
+        });
+    }
+    getData();
+  }, [limit, offset]);
+
+  return (
+    <Router>
+      <div className="content flex-center">
+        <div className="list-div">
+          <ul className="list-group">
+            {data.map((item, i) => (
+              <li key={i} className="list-group-item">
+                <span style={{ float: "left" }}>{i + 1}</span>
+                <Link to={`/pokemon?name=${item.name}`}>{item.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </Router>
+  );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Header() {
-  return <div className="header flex-center">Header</div>;
+  return (
+    <div className="header flex-center">
+      <h2>Pokemon List</h2>
+    </div>
+  );
 }
