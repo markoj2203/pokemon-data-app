b113fcb66e9012c271ee402a9bac9e1494cdfcae
unknown <markoj2203@gmail.com>
2020-12-02 10:14:46 +0100

    setup starting envirement

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Content from "./Content";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="flex-center">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
