1a2cedc7d4835eb599560cf3a3a40bd52a689982
unknown <markoj2203@gmail.com>
2020-12-05 17:47:28 +0100

    code correction

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-//import goToPokemonPage from "./reducers";
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
